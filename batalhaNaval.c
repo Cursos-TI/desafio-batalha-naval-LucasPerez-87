887245b11a1b274fb4b8cb7310e106d6469cc0d9
LucasPerez-87 <lucasperezcm@gmail.com>
2025-08-21 17:23:30 +0000

    Atualização codigo batalha naval Nível Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,43 +4,62 @@ int main() {
     int tabuleiro[10][10];
     int i, j;
 
-    // inicializar tabuleiro com 0
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
+    // Inicializar tabuleiro com água (0)
+    for (i = 0; i < 10; i++)
+        for (j = 0; j < 10; j++)
             tabuleiro[i][j] = 0;
+
+    // ----------------------
+    // Posicionar navios (3)
+    // ----------------------
+    tabuleiro[2][4] = 3; tabuleiro[2][5] = 3; tabuleiro[2][6] = 3; // horizontal
+    tabuleiro[6][7] = 3; tabuleiro[7][7] = 3; tabuleiro[8][7] = 3; // vertical
+    tabuleiro[0][0] = 3; tabuleiro[1][1] = 3; tabuleiro[2][2] = 3; // diagonal principal
+    tabuleiro[0][9] = 3; tabuleiro[1][8] = 3; tabuleiro[2][7] = 3; // diagonal secundária
+
+    // ----------------------
+    // Habilidades especiais
+    // ----------------------
+    int tamanho = 5; // tamanho das matrizes de habilidade
+
+    // Função para aplicar área de habilidade (5) sem sobrescrever navios
+    void aplicarHabilidade(int origemLinha, int origemColuna, int habilidade[5][5]) {
+        for (i = 0; i < tamanho; i++) {
+            for (j = 0; j < tamanho; j++) {
+                int linhaTab = origemLinha - 2 + i;
+                int colTab = origemColuna - 2 + j;
+                if (linhaTab >= 0 && linhaTab < 10 && colTab >= 0 && colTab < 10 && habilidade[i][j] == 1) {
+                    if (tabuleiro[linhaTab][colTab] != 3) // não sobrescrever navios
+                        tabuleiro[linhaTab][colTab] = 5;
+                }
+            }
         }
     }
 
-    // Navio horizontal (linha 2, colunas 4-6)
-    int linhaH = 2;
-    int colunaH = 4;
-    tabuleiro[linhaH][colunaH] = 3;
-    tabuleiro[linhaH][colunaH+1] = 3;
-    tabuleiro[linhaH][colunaH+2] = 3;
+    // Cone
+    int cone[5][5] = {0};
+    for (i = 0; i < tamanho; i++)
+        for (j = 0; j < tamanho; j++)
+            if (j >= 2 - i && j <= 2 + i) cone[i][j] = 1;
+    aplicarHabilidade(2, 5, cone);
 
-    // Navio vertical (coluna 7, linhas 6-8)
-    int linhaV = 6;
-    int colunaV = 7;
-    tabuleiro[linhaV][colunaV] = 3;
-    tabuleiro[linhaV+1][colunaV] = 3;
-    tabuleiro[linhaV+2][colunaV] = 3;
+    // Cruz
+    int cruz[5][5] = {0};
+    for (i = 0; i < tamanho; i++) { cruz[2][i] = 1; cruz[i][2] = 1; }
+    aplicarHabilidade(5, 2, cruz);
 
-    // Navio diagonal principal (começa em [0,0], [1,1], [2,2])
-    int linhaD1 = 0;
-    int colunaD1 = 0;
-    tabuleiro[linhaD1][colunaD1] = 3;
-    tabuleiro[linhaD1+1][colunaD1+1] = 3;
-    tabuleiro[linhaD1+2][colunaD1+2] = 3;
+    // Octaedro (losango)
+    int octaedro[5][5] = {0};
+    for (i = 0; i < tamanho; i++)
+        for (j = 0; j < tamanho; j++)
+            if (i + j >= 2 && i + j <= 6 && j - i <= 2 && i - j <= 2)
+                octaedro[i][j] = 1;
+    aplicarHabilidade(7, 7, octaedro);
 
-    // Navio diagonal secundária (começa em [0,9], [1,8], [2,7])
-    int linhaD2 = 0;
-    int colunaD2 = 9;
-    tabuleiro[linhaD2][colunaD2] = 3;
-    tabuleiro[linhaD2+1][colunaD2-1] = 3;
-    tabuleiro[linhaD2+2][colunaD2-2] = 3;
-
-    // imprimir tabuleiro
-    printf("***Jogo Batalha Naval***\n");
+    // ----------------------
+    // Imprimir tabuleiro
+    // ----------------------
+    printf("***Jogo Batalha Naval - Nível Mestre***\n");
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
             printf("%d ", tabuleiro[i][j]);
